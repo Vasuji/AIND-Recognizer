1cca6e0e2fc1d64ddeb01bd34a55ddb0c7ace592
Dibakar <sigdeldkr@outlook.com>
2017-07-18 19:26:20 -0400

    re-submit
    re-submit

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -13,10 +13,14 @@ class ModelSelector(object):
     base class for model selection (strategy design pattern)
     '''
 
-    def __init__(self, all_word_sequences: dict, all_word_Xlengths: dict, this_word: str,
+    def __init__(self, all_word_sequences: dict,
+                 all_word_Xlengths: dict,
+                 this_word: str,
                  n_constant=3,
-                 min_n_components=2, max_n_components=10,
+                 min_n_components=2,
+                 max_n_components=10,
                  random_state=14, verbose=False):
+        
         self.words = all_word_sequences
         self.hwords = all_word_Xlengths
         self.sequences = all_word_sequences[this_word]
@@ -36,11 +40,17 @@ class ModelSelector(object):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
         # warnings.filterwarnings("ignore", category=RuntimeWarning)
         try:
-            hmm_model = GaussianHMM(n_components=num_states, covariance_type="diag", n_iter=1000,
-                                    random_state=self.random_state, verbose=False).fit(self.X, self.lengths)
+            
+            hmm_model = GaussianHMM(n_components=num_states,
+                                    covariance_type="diag",
+                                    n_iter=1000,
+                                    random_state=self.random_state,
+                                    verbose=False).fit(self.X, self.lengths)
+            
             if self.verbose:
                 print("model created for {} with {} states".format(self.this_word, num_states))
             return hmm_model
+        
         except:
             if self.verbose:
                 print("failure on {} with {} states".format(self.this_word, num_states))
@@ -65,42 +75,79 @@ class SelectorConstant(ModelSelector):
 
 
 class SelectorBIC(ModelSelector):
-     """ select the model with the lowest Bayesian Information Criterion(BIC) score
-    http://www2.imm.dtu.dk/courses/02433/doc/ch6_slides.pdf
-    Bayesian information criteria: BIC = -2 * logL + p * logN
+    """ 
+    Abbreviations:
+        - BIC - Baysian Information Criterion
+    
+    Objective: 
+        select the model with the lowest Bayesian Information Criterion(BIC) score
+         
+    About BIC:
+        - SelectorBIC accepts argument of ModelSelector instance of base class
+          with attributes such as: this_word, min_n_components, max_n_components
+        - Maximises the likelihood of data whilst penalising large-size models
+        - Used to scoring model topologies by balancing fit
+          and complexity within the training set for each word
+        - Avoids using CV by instead using a penalty term   
+      
+    References:
+        [0] - http://www2.imm.dtu.dk/courses/02433/doc/ch6_slides.pdf
+        [1] - https://en.wikipedia.org/wiki/Hidden_Markov_model#Architecture
+        [2] - https://stats.stackexchange.com/questions/12341/number-of-parameters-in-markov-model
+        [3] - https://discussions.udacity.com/t/number-of-parameters-bic-calculation/233235/8
+        [4] - http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.58.6208&rep=rep1&type=pdf   
     """
 
+        
+    def bic_score(self, num_states):
+        
+        """
+         BIC Equation:  BIC = -2 * log L + p * log N 
+            L : is the likelihood of the fitted model
+            p : is the number of parameters
+            N : is the number of data points
+                    
+        Notes:
+          -2 * log L    -> decreases with higher "p"
+          p * log N     -> increases with higher "p"
+          N > e^2 = 7.4 -> BIC applies larger "penalty term" in this case
+            
+        """
+        
+        model = self.base_model(num_states)
+        log_likelihood = model.score(self.X, self.lengths)
+        number_of_parameters = num_states ** 2 + 2 * num_states * model.n_features - 1
+        score = -2 * log_likelihood + number_of_parameters * np.log(num_states)
+        return score, model
+        
 
     def select(self):
-         """ select the best model for self.this_word based on
+        """ select the best model for self.this_word based on
         BIC score for n between self.min_n_components and self.max_n_components
         :return: GaussianHMM object
         """
         warnings.filterwarnings("ignore", category=DeprecationWarning)
         # TODO implement model selection based on BIC scores
         
-        
-        # BIC : −2 log L + p log N
-        # L : is the likelihood of the fitted model
-        # p : is the number of parameters
-        # N : is the number of data points
-        
-        bic_scores = []
+        """ Selection using BIC Model:
+            - Lower the BIC score the "better" the model.
+            - Loop from min_n_components to max_n_components
+            - Find the lowest BIC score as the "better" model.
+        """
+      
         try:
-            for n in self.n_components:
-                
-                model = self.base_model(n)
-                log_likelihood = model.score(self.X, self.lengths)
-                number_of_parameters = n ** 2 + 2 * n * model.n_features - 1
-                bic_score = -2 * log_likekihood + number_of_parameters * math.log(n)
-                bic_scores.append(bic_score)
-        except Exception as e:
-            pass
-        
+            
+            best_score = float("Inf") 
+            best_model = None
 
-        states = self.n_components[np.argmax(bic_scores)] if bic_scores else self.n_constant
-        return self.base_model(states)
+            for num_states in range(self.min_n_components, self.max_n_components + 1):
+                score, model = self.bic_score(num_states)
+                if score < best_score:
+                    best_score, best_model = score, model
+            return best_model
 
+        except:
+            return self.base_model(self.n_constant)
     
   
     
@@ -113,47 +160,109 @@ class SelectorBIC(ModelSelector):
     
 class SelectorDIC(ModelSelector):
     
-     ''' select best model based on Discriminative Information Criterion
-        Biem, Alain. "A model selection criterion for classification: 
+    """
+      Abbreviations:
+        - DIC - Discriminative Information Criterion
+    
+      Objective: 
+       "select best model based on Discriminative Information Criterion
+        Biem, Alain. A model selection criterion for classification: 
         Application to hmm topology optimization."
-        Document Analysis and Recognition, 2003. Proceedings.
-        Seventh International Conference on. IEEE, 2003.
-        http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.58.6208&rep=rep1&type=pdf
-         DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
-    '''
+    
+      About DIC:
+        - In DIC we need to find the number of components where the difference is largest.
+        The idea of DIC is that we are trying to find the model that gives a
+        high likelihood (small negative number) to the original word and
+        low likelihood (very big negative number) to the other words
+        
+        - In order to get an optimal model for any word, we need to run the model on all
+        other words so that we can calculate the formula
+        
+        - DIC is a scoring model topology that scores the ability of a
+        training set to discriminate one word against competing words.
+        It provides a "penalty" if model likelihoods
+        for non-matching words are too similar to model likelihoods for the
+        correct word in the word set (rather than using a penalty term for
+        complexity like in BIC)
+        
+        - Task-oriented model selection criterion adapts well to classification
+        problems
+        
+        - Classification task accounts for Goal of model  (differs from BIC)
+        
+         
+        References:
+        [0] - http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.58.6208&rep=rep1&type=pdf
+         
+    """
+        
+        
+    def dic_score(self, num_states):
+        """
+          DIC Equation:
+            DIC = log(P(X(i)) - 1/(M - 1) * sum(log(P(X(all but i))
+            i <- current_word
+            DIC = log likelihood of the data belonging to model
+                - avg of anti-log likelihood of data X and model M
+                = log(P(original word)) - average(log(P(other words)))
+               
+            where anti-log likelihood means likelihood of data X and model M belonging
+            to competing categories where log(P(X(i))) is the log-likelihood of the fitted
+            model for the current word.
+        
+          Note:
+            - log likelihood of the data belonging to model
+            - anti_log_likelihood of data X vs model M
+
+        """
+        
+        model = self.base_model(num_states)
+        logs_likelihood = []
+        
+        for word, (X, lengths) in self.hwords.items():
+            
+            # likelihood of current word
+            if word == self.this_word:
+                current_word_likelihood = model.score(self.X, self.lengths)
+                
+            # if word != self.this_word:
+            # likelihood of remaining words
+            else:
+                logs_likelihood.append(model.score(X, lengths))
+             
+        score = current_word_likelihood - np.mean(logs_likelihood)
+        
+        return score, model
+    
+    
 
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
         
         # TODO implement model selection based on DIC scores
-        # DIC score = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
+        
+        """Selection using DIC Model:
+            - Higher the DIC score the "better" the model.
+            - SelectorDIC accepts argument of ModelSelector instance of base class
+              with attributes such as: this_word, min_n_components, max_n_components,
+            - Loop from min_n_components to max_n_components
+            - Find the highest BIC score as the "better" model.
+        """
 
-        
-        dic_scores = []
-        logs_likelihood = []
-        
         
         try:
-            for n_component in self.n_components:
-                model = self.base_model(n_component)
-                logs_likelihood.append(model.score(self.X, self.lengths))
-                
-            sum_logs_likelihood = sum(logs_likelihood)
-            m = len(self.n_components)
+            best_score = float("-Inf")
+            best_model = None
             
-            for log_l in logs_likelihood:
-                
-                # DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
-                
-                other_words_likelihood = (sum_logs_likelihood - log_l) / (m - 1)
-                dic_scores.append(log_l - other_words_likelihood)
-                
-        except Exception as e:
-            pass
+            for num_states in range(self.min_n_components, self.max_n_components+1):
+                score, model = self.dic_score(num_states)
+                if score > best_score:
+                    best_score = score
+                    best_model = model
+            return best_model   
 
-        
-        states = self.n_components[np.argmax(dic_scores)] if dic_scores else self.n_constant
-        return self.base_model(states)
+        except:
+            return self.base_model(self.n_constant)
 
     
     
@@ -167,43 +276,107 @@ class SelectorDIC(ModelSelector):
     
     
 class SelectorCV(ModelSelector):
-    ''' select best model based on average log Likelihood of cross-validation folds'''
+    """ 
+        Abbreviations:
+            - CV - Cross-Validation
+            
+            Objective: select best model based on average log Likelihood of cross-validation folds
+        About CV:
+            - Scoring the model simply using Log Likelihood calculated from
+              feature sequences it trained on, we expect more complex models
+              to have higher likelihoods, but doesn't inform us which would
+              have a "better" likelihood score on unseen data. The model will
+              likely overfit as complexity is added.
+              
+            - Estimate the "better" Topology model using only training data
+              by comparing scores using Cross-Validation (CV).
+              
+            - CV technique includes breaking-down the training set into "folds",
+              rotating which fold is "left out" of the training set.
+              The fold that is "left out" is scored for validation.
+              Use this as a proxy method of finding the
+             "best" model to use on "unseen data".
+             
+              e.g. Given a set of word sequences broken-down into three folds
+              using scikit-learn Kfold class object.
+              
+            - CV useful to limit over-validation
+    
+        References:
+            [0] - http://scikit-learn.org/stable/modules/generated/sklearn.model_selection.KFold.html
+            [1] - https://www.r-bloggers.com/aic-bic-vs-crossvalidation/
+            
+    """
+    
+    def cv_score(self, num_states):
+        """
+        Calculate the average log likelihood of cross-validation folds using the KFold class
+        :return: tuple of the mean likelihood and the model with the respective score
+        
+        CV Equation:
+        
+        
+        """
+        fold_scores = []
+        split_method = KFold(n_splits = 3, shuffle = True, random_state = 1)
+        
+        
+        for train_idx, test_idx in split_method.split(self.sequences):
+            # Training sequences split using KFold are recombined
+            self.X, self.lengths = combine_sequences(train_idx, self.sequences)
+            # Get test sequences
+            test_X, test_length = combine_sequences(test_idx, self.sequences)
+            # Record each model score
+            model = self.base_model(num_states)
+            fold_scores.append(model.score(test_X, test_length))
+            
+        # Compute mean of all fold scores
+        score = np.mean(fold_scores)
+            
+        return score, model
 
+    
     def select(self):
+        
+        """ select the best model for self.this_word based on
+        CV score for n between self.min_n_components and self.max_n_components
+        It is based on log likehood
+        :return: GaussianHMM object
+        
+        Selection using CV Model:
+            - Higher the CV score the "better" the model.
+            - Select "best" model based on average log Likelihood
+              of cross-validation folds
+            - Loop from min_n_components to max_n_components
+            - Find the higher score(logL), the higher the better.
+            - Score that is "best" for SelectorCV is the
+              average Log Likelihood of Cross-Validation (CV) folds.
+          
+        """
+        
         warnings.filterwarnings("ignore", category=DeprecationWarning)
-        
-        # TODO implement model selection using CV
-        # check based on cross validation
-        
-        mean_scores = []
-        
-        # Save reference to 'KFold' in variable as shown in notebook
-        split_method = KFold()
-        
-        
+
         try:
-            for n_component in self.n_components:
-                model = self.base_model(n_component)
-                # Fold and calculate model mean scores
-                fold_scores = []
-                
-                for _, test_idx in split_method.split(self.sequences):
-                    
-                    # Get test sequences
-                    test_X, test_length = combine_sequences(test_idx, self.sequences)
-                    
-                    # Record each model score
-                    fold_scores.append(model.score(test_X, test_length))
-
-                # Compute mean of all fold scores
-                mean_scores.append(np.mean(fold_scores))
-                
-        except Exception as e:
-            pass
-
+            best_score = float("Inf")
+            best_model = None
+            
+            for num_states in range(self.min_n_components, self.max_n_components+1):
+                score, model = self.cv_score(num_states)
+                if score < best_score:
+                    best_score = score
+                    best_model = model
+            return best_model
+        except:
+            return self.base_model(self.n_constant)
         
-        states = self.n_components[np.argmax(mean_scores)] if mean_scores else self.n_constant
-        return self.base_model(states)
+        
+        
+    
+    
+    
+    
+    
+    
     
     
     
